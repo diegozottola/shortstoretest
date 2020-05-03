256894ae70a9033093a304b3e9ccc875e75e49ee
diegozottola <diego.zottola@atenatesting.com>
2020-05-03 02:59:56 -0300

    first push

---
+++ src/test/java/Compra.java
@@ -0,0 +1,43 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+public class Compra {
+    public RemoteWebDriver firefoxDriver;
+
+    @BeforeTest
+    public void abrirDriver() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("80.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+         firefoxDriver = new RemoteWebDriver(
+
+        URI.create("http://192.168.245.3:4444/wd/hub").toURL(),
+                capabilities
+        );
+        firefoxDriver.manage().window().maximize();
+    }
+
+
+
+    @AfterTest
+    public void cerrarDriver() {
+        //Cerrar ventana, apagar driver
+        firefoxDriver.quit();
+    }
+}
---
+++ src/test/java/test4.java
@@ -0,0 +1,56 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class test4 extends Compra{
+
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Verify home page title")
+    @Story("Test verify base page title")
+    @Test
+    public void hacer_una_compra() {
+        //Abrimos una URL
+        firefoxDriver.get("http://www.automationpractice.com/index.php");
+
+        //Crear elementos Web
+        WebElement labelSignIn = firefoxDriver.findElement(By.xpath("//*[@title='Log in to your customer account']"));
+        labelSignIn.click();
+
+        //Cargamos el campo de email
+        WebElement campoEmail = firefoxDriver.findElement(By.cssSelector("#email"));
+        campoEmail.sendKeys("juantor16@gmail.com");
+
+        //Cargamos el campo de contraseña
+        WebElement campoPassword = firefoxDriver.findElement(By.cssSelector("#passwd"));
+        campoPassword.sendKeys("Argentina1");
+
+        //Hacemos click en Sign in
+        WebElement botonSignIn = firefoxDriver.findElement(By.cssSelector("#SubmitLogin"));
+        botonSignIn.click();
+
+        //Seleccionamos la pestaña de "T-shirts"
+        WebElement etiquetaTshirts = firefoxDriver.findElement(By.xpath("//*[@class='sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li/a[contains(text(),\"T-shirts\")]"));
+        etiquetaTshirts.click();
+
+        //Creamos una acción
+        Actions action = new Actions(firefoxDriver);
+
+        //Encontramos un elemento de la imagen que queremos comprar
+        WebElement we = firefoxDriver.findElement(By.xpath("//img[@title='Faded Short Sleeve T-shirts']"));
+
+        //Movemos el cursor hacia el boton de agregar al carrito
+        action.moveToElement(we).moveToElement(firefoxDriver.findElement(By.cssSelector(".ajax_add_to_cart_button.btn.btn-default"))).click().build().perform();
+
+    }
+
+
+
+
+}
